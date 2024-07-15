9417182b4453d4bde6acc704e27bcef6a7c8ca73
sar <sar>
2024-07-15 22:23:56 +0000

    script::nvim_setup

---
+++ nixos_nvim_install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable unstable
+nix-channel --update unstable
+nix-env -iA unstable.neovim
+nix-env -iA unstable.neovim-qt
+nix-env -iA nixos.xterm
+
+mkdir -p ~/.config/nvim
+cd ~/.config/nvim
+git clone https://github.com/sar/nvim-config .
+./git_sync.sh
+
+
